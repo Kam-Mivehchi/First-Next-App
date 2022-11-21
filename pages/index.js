285bb9ce7302ba502520d9a3367a9669dcb5e31d
kmivehchi-xRoird <kammivehchi@gmail.com>
2022-11-21 11:29:37 -0800

    initial commit Navigation and images complete

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,8 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <Link href="https://nextjs.org">Nexxt.js!</Link>
         </h1>
+        <h2>
+          <Link href="/posts/first-post">firstpost</Link>
+        </h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
