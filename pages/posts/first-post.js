40497af0d287e05cd1b73a140b72a6ada5b0c105
kmivehchi-xRoird <kammivehchi@gmail.com>
2022-11-21 14:22:52 -0800

    progress completed css, moving on the rendering

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,27 +1,28 @@
-import React from 'react'
 import Link from 'next/link';
 import Head from 'next/head'
 import Script from 'next/script';
-const FirstPost = () => {
+import Layout from '../../components/layout'
+
+
+export default function FirstPost() {
    return (
       <>
-         <Head>
-            <title>First Post</title>
-         </Head>
-         <Script
-            src="https://connect.facebook.net/en_US/sdk.js"
-            strategy="lazyOnload"
-            onLoad={() =>
-               console.log(`script loaded correctly, window.FB has been populated`)
-            }
-         />
-         <h1>first-post</h1>
-         <h2>
-            <Link href="/">Back to home</Link>
-         </h2>
+         <Layout>
+            <Head>
+               <title>First Post</title>
+            </Head>
+            <Script
+               src="https://connect.facebook.net/en_US/sdk.js"
+               strategy="lazyOnload"
+               onLoad={() =>
+                  console.log(`script loaded correctly, window.FB has been populated`)
+               }
+            />
+
+         </Layout>
 
       </>
    )
 }
 
-export default FirstPost
+
